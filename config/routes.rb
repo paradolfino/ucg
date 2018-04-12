2a2843cbcd0b82108ba721761bc58890a1330b59
Viktharien <viktharien@zoho.com>
2018-04-12 22:16:41 +0000

    gen model and controller, setup views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'accounts#index'
   get 'accounts/:id' => 'accounts#show', as: 'account'
+  get 'accounts/:id' => 'accounts#edit'
   put 'accounts/:id' => 'accounts#update'
 end
